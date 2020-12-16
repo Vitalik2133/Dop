91b375d9e0c1b8e6867efab712c6840ef23a7912
Vitalik2133 <76100797+Vitalik2133@users.noreply.github.com>
2020-12-16 15:23:31 +0500

    Add files via upload
    first4

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button myBut = new Button();
+            myBut.Width = 150;
+            myBut.Height = 40;
+            myBut.HorizontalAlignment = HorizontalAlignment.Left;
+            myBut.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Purple);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox1.Text;
+            if (text != "")
+            {
+                MessageBox.Show(text);
+            }
+        }
+    }
+}
